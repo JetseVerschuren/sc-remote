e51e69c2d9b7ca7c43fe8dc5cbd0ac145867aa31
Jetse Verschuren <j.verschuren@student.utwente.nl>
2023-01-21 23:07:59 +0100

    Mostly finished

---
+++ sw.js
@@ -0,0 +1,32 @@
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+const cacheName = 'lolidk';
+const appShellFiles = [
+  '/',
+  'style.css',
+  'manifest.json',
+  'fonts/roboto.css',
+  'fonts/Roboto-Regular.ttf',
+  'fonts/Roboto-Medium.ttf',
+  'icons/logo.svg',
+  'icons/logo192.png',
+  'icons/logo512.png',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(appShellFiles);
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) { return r; }
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    const response = await fetch(e.request);
+    return response;
+  })());
+});
